94d8a11db2303d0016b0659e9a6ffdb912820dc5
angus <junshu.jiang@kaust.edu.sa>
2022-11-19 15:37:30 +0300

    visualize the correlation

--- helper.R
+++ helper.R
@@ -41,21 +41,128 @@ visualize_frame_raw<- function(value_2d,lantitude=NULL,latidude=NULL,timestamp="
 
 helper_describe<-function(array){
 
-valid_element_number<-length(array[!is.na(array)])
-valid_percentage<-(valid_element_number/length(array)*100)
-sum_table<-summary(array[!is.na(array)],na.rm=TRUE)
-sum_table["Valid.Ent"]<-valid_element_number
-sum_table["Valid.percent"]<-valid_percentage
-sum_table["Std"]<-sqrt(var(array,na.rm=TRUE))
-userful_entry<-array[!is.nan(array)]
+  valid_element_number<-length(array[!is.na(array)])
+  valid_percentage<-(valid_element_number/length(array)*100)
+  sum_table<-summary(array[!is.na(array)],na.rm=TRUE)
+  sum_table["Valid.Ent"]<-valid_element_number
+  sum_table["Valid.percent"]<-valid_percentage
+  sum_table["Std"]<-sqrt(var(array,na.rm=TRUE))
+  userful_entry<-array[!is.nan(array)]
 
 
 
-hist(userful_entry,
-  xlab = "log(mg/m3)",
-  breaks = 100
-) 
+  hist(userful_entry,
+    breaks = 100
+  ) 
 
 
-print(sum_table)
+  print(sum_table)
+}
+
+
+## linear regression for 3D and reduce to 2d trend matrix
+reducer_trend<-function(T_array,dataType="Chl",thres_min_data_point){
+  results<-list()
+  length_latitude<-dim(T_array)[2]
+  length_longitude<-dim(T_array)[1]
+  length_timestamp<-dim(T_array)[3]
+
+  for(latitude_i in 1:length_latitude) {
+
+    for (longitude_i in 1:length_longitude){
+      raw_t=seq(1,length_timestamp)
+      raw_y=T_array[longitude_i,latitude_i,1:length_timestamp]
+
+      t<-raw_t[!is.na(raw_y)]
+      y<-raw_y[!is.na(raw_y)]
+      if (length(t)>thres_min_data_point){
+        data=data.frame(t,y)
+        ## specify model based on datatype
+        if (dataType=="Chl"){
+          model<-gam(y~t,family=Gamma(link = "log"),data=data) 
+        }
+        else if(dataType=="SST"){
+          model<-gam(y~t,data=data) 
+        }
+        else{
+          stopifnot(FALSE)
+        }
+        results<-append(results,model$coefficients["t"])
+      }
+      else{
+        results<-append(results,NA)
+      }
+    }
+
+  }
+  matrix<-unlist(results)
+  dim(matrix) <- c(length_longitude, length_latitude)
+  return(matrix)
+}
+
+
+# take one 3d arrayday reduce the 3d data to 2d
+# the two tensor should have the same shape
+reducer2_correlation<-function(T_array1,T_array2){
+    
+
+    stopifnot(dim(T_array1)[3]==dim(T_array2)[3])
+    d3_length<-dim(T_array1)[3]
+    T1_mean<-rowMeans(T_array1,na.rm=TRUE,dims=2)
+    T2_mean<-rowMeans(T_array2,na.rm=TRUE,dims=2)
+
+    nominator<-rowMeans(rray_subtract(T_array1,T1_mean)*rray_subtract(T_array2,T2_mean)
+        ,na.rm=TRUE,dims=2)
+
+    var1<-rowMeans(rray_subtract(T_array1,T1_mean)**2,na.rm=TRUE,dims=2)
+    var2<-rowMeans(rray_subtract(T_array2,T2_mean)**2,na.rm=TRUE,dims=2)
+
+    denominator<-(var1*var2)**0.5
+
+    return(nominator/denominator)
+
+}
+
+##deprecated since no acceration observed 
+parallel_trend_linear_regression<-function(array,dataType,thres_min_data_point){
+    raw_t=seq(1,length_timestamp)
+    t<-raw_t[!is.na(array)]
+    y<-array[!is.na(array)]
+    if (length(t)>thres_min_data_point){
+        if (dataType=="Chl"){
+            model<-gam(y~t,family=Gamma(link = "log"),data=data) 
+        }
+        else if(dataType=="SST"){
+            model<-gam(y~t,data=data) 
+        }
+        else{
+            stopifnot(FALSE)
+        }
+        return(results,model$coefficients["t"])
+      }
+    else{
+        return(NA)
+        
+    }
+
+    
+}
+##deprecated since no acceration observed 
+reducer_trend_parallel<-function(T_array,dataType="Chl",thres_min_data_point){
+  length_latitude<-dim(T_array)[2]
+  length_longitude<-dim(T_array)[1]
+  length_timestamp<-dim(T_array)[3]
+
+  array_list<-list()
+  for(latitude_i in 1:length_latitude) {
+
+      for (longitude_i in 1:length_longitude){
+          array_list<-append(array_list,T_array[longitude_i,latitude_i,1:length_timestamp])
+      }
+      }
+
+  print(length(array_list))
+  result<-foreach(x=1:length(array_list),.combine = ) %do% 
+      parallel_trend_linear_regression(array_list[x],dataType,thres_min_data_point)
+  print(class(result))
 }
